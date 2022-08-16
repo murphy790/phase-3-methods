4c9f7df12740d4f8ba702c9072b12477a17464b5
Alex Mwaura <alexmwaura36@gmail.com>
2022-08-16 17:49:09 +0300

    assignment completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet("Naureen")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default()
+
+
+def add(num1=1,num2=2)
+    return num1 + num2
+end
+add
+
+def halve(num1)
+    if num1.class != Integer
+        return nil
+    end
+    num1/2
+end
+
+
+
